139ecdf515eac605a7b6306f785671a4822b0d41
Artur Ardai <krkerdo@gmail.com>
2022-10-15 14:31:02 +0300

    ye

--- to.java
+++ to.java
@@ -13,4 +13,16 @@ public class to {
         a[n]=result;
         return result;
     }
+
+    public static int fii(int n)
+    {
+        if(n==1||n==2) return 1;
+        int[] a=new int[n+1]; 
+        a[1]=1;
+        a[2]=1;
+        for (int i = 3; i < a.length; i++) {
+            a[i]=a[i-1]+a[i-2];
+        }
+        return a[n];
+    }
 }
