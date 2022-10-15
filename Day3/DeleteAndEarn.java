113f1319975c60f3e4932effaa336f9232470572
Artur Ardai <krkerdo@gmail.com>
2022-10-15 18:08:04 +0300

    no

--- Day3/DeleteAndEarn.java
+++ Day3/DeleteAndEarn.java
@@ -4,6 +4,19 @@ package Day3;
 
 public class DeleteAndEarn {
     public int deleteAndEarn(int[] nums) {
-       return 0;
+       int[] count=new int [10001];
+       for (int i : nums) {
+        count[i]+=i;
+       }
+       int prev=0;
+       int cur=0,next=0;
+       for (int i = 0; i < count.length; i++) {
+        next=Math.max(prev+count[i], cur);
+        prev=cur;
+        cur=next;
+       }
+
+
+        return next;
     }
 }
